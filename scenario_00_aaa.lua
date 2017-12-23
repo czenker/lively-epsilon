17b4e08c2b45ed0a39938898bc24a367cd22a74d
Christian Zenker <dev@xopn.de>
2017-12-23 16:45:33 +0100

    add a demo for the patrol script

--- scenario_00_aaa.lua
+++ scenario_00_aaa.lua
@@ -154,35 +154,197 @@ function init()
     end)
 
     addGMFunction("Test Patrol", function()
-        local function onHeading(ship)
-            ChitChat.say(ship, "Let's go")
-        end
-        local function onArrival(ship)
-            ChitChat.say(ship, "Arrived")
-        end
+        local northPole = Station:enrich(SpaceStation():setTemplate('Small Station'):setFaction("Human Navy"))
+        local station1 = Station:enrich(SpaceStation():setTemplate('Medium Station'):setFaction("Human Navy"))
+        local station2 = Station:enrich(SpaceStation():setTemplate('Medium Station'):setFaction("Human Navy"))
+        local station3 = Station:enrich(SpaceStation():setTemplate('Medium Station'):setFaction("Human Navy"))
+        local station4 = Station:enrich(SpaceStation():setTemplate('Medium Station'):setFaction("Human Navy"))
 
-        for i=3,13,1 do
-            Ship:patrol(CpuShip():setTemplate("Adder MK5"):setPosition(0, i * 5000):setRotation(0):setFaction("Human Navy"):setImpulseMaxSpeed(250):setRotationMaxSpeed(50), {
+        setCirclePos(northPole, 0, -30000, 0, 20000)
+        setCirclePos(station1, 0, -30000, 72, 20000)
+        setCirclePos(station2, 0, -30000, 144, 20000)
+        setCirclePos(station3, 0, -30000, 216, 20000)
+        setCirclePos(station4, 0, -30000, 288, 20000)
+
+        northPole:setDescription("A mysterious place")
+
+        Cron.once(function()
+            local rudolph = Ship:enrich(
+                CpuShip():setTemplate("MU52 Hornet"):setRotation(0):setFaction("Human Navy")
+                :setDescription("A ship with strange readings. It clearly does not satisfy any of the safety regulations put out by the Human Navy.")
+            ):setCallSign("XMAS17"):setCommsFunction(function()
+                setCommsMessage("Ho! Ho! Ho!\n\nHave you been naughty or nice this year?")
+                addCommsReply("Naughty", function()
+                    setCommsMessage("I can't accept that. You have to be punished.")
+                    Cron.once(function()
+                        local x,y = player:getPosition()
+                        setCirclePos(CpuShip():setTemplate("Atlantis X23"):setFaction("Ghosts"), x, y, math.random(0, 360), 5000):orderAttack(player)
+                    end, 2)
+                end)
+                addCommsReply("Nice", function()
+                    setCommsMessage("That's great. Keep it that way.")
+                end)
+            end):setWarpDrive(true)
+
+            rudolph.captain.firstName = "Santa"
+            rudolph.captain.lastName = "Claus"
+
+            Util.spawnAtStation(northPole, rudolph)
+
+            Ship:patrol(rudolph, {
                 {
-                    target = {-5000, i * 5000 },
-                    onHeading = onHeading,
-                    onArrival = onArrival,
-                    delay = i - 3
-                },
-                {
-                    target = {5000, i * 5000 },
-                    onHeading = onHeading,
-                    onArrival = onArrival,
-                    delay = i - 3
+                    target = station1,
+                    onHeading = function()
+                        ChitChat.say(rudolph, "Ho! Ho! Ho!")
+                    end,
+                    onArrival = function()
+                        ChitChat.converse({
+                            {rudolph, "Are you Timmy?"},
+                            {"Timmy", "Mommy, who is that weirdo with the beard?"},
+                            {"Mom", "It's Santa!"},
+                            {rudolph, "Ho! Ho! Ho!"},
+                            {"Timmy", "Why are you not coming through the chimney?"},
+                            {rudolph, "Because you live on a Space Station and it has a central heating unit."},
+                            {"Timmy", "But how does Rudolph breath in space?"},
+                            {rudolph, "You ask way to many questions, Timmy."},
+
+                        })
+                    end,
+                    delay = 30
+                },{
+                    target = station2,
+                    onArrival = function()
+                        ChitChat.converse({
+                            {rudolph, "Marty, I brought you that Flux Capacitator, that you always wanted."},
+                            {"Marty", "Yeah, you are like 60 years late, you dumbass."},
+                        })
+                    end,
+                    delay = 30
+                },{
+                    target = station3,
+                    onArrival = function()
+                        ChitChat.converse({
+                            {rudolph, "You have been very, very naughty."},
+                            {"Kenny", "**unintelligable mumbling**"},
+                            {station3, "*pew pew pew*"},
+                            {"Kyle", "Oh my God! He killed Kenny!"},
+                            {"Stan", "You bastard!"}
+                        })
+                    end,
+                    delay = 30
+                },{
+                    target = northPole,
+                    onHeading = function()
+                        ChitChat.converse({
+                            {rudolph, "That's all for this year."},
+                            {rudolph, "Now for some Home brewed Egg Nogg at home."},
+                        })
+                    end,
+                    onArrival = function()
+                        ChitChat.say(rudolph, "Finally home.")
+                        Cron.once(function() rudolph:destroy() end, 10)
+
+                        northPole:setCommsFunction(function()
+                            setCommsMessage("Have a Merry Christmas and a great year 2018, folks.")
+                        end)
+                        northPole:openCommsTo(player)
+                    end,
+                    delay = 999
                 },
             })
-        end
+        end, 100)
+
+        Cron.regular("universe_travel", function()
+            local source = Util.random({station1, station2, station3, station4})
+            local destination = Util.random({station1, station2, station3, station4})
+
+            if source ~= destination then
+                narrative(source, destination)
+            end
+        end, 30)
 
         removeGMFunction("Test Patrol")
     end)
-
 end
 
 function update(delta)
     Cron.tick(delta)
+end
+
+-- boiler plate code – this will be improved
+local narratives = {
+
+    -- shipping cargo
+    function(fromStation, toStation)
+        local cargoList = {
+            "birthday supplies",
+            "wedding cake",
+            "expensive art pieces",
+            "fine meat",
+            "newspapers",
+            "latest fashion",
+            "medicine",
+            "robots",
+        }
+
+        local ship = Ship:enrich(CpuShip():setTemplate("Goods Freighter " .. math.random(1,5)):setWarpDrive(true):setFaction("Human Navy"))
+        Util.spawnAtStation(fromStation, ship)
+        local cargo = Util.random(cargoList)
+
+        Ship:patrol(ship, {
+            {
+                target = toStation,
+                onHeading = function()
+                    ship:setDescription("This ship brings " .. cargo .. " to " .. toStation:getCallSign() .. ".")
+                end,
+                onArrival = function()
+                    ship:destroy()
+                end
+            }
+        })
+    end,
+    -- transporting people
+    function(fromStation, toStation)
+        local passengerList = {
+            {"party goers", "concert"},
+            {"party goers", "night club"},
+            {"students", "spring break celebration"},
+            {"rich kids", "casino"},
+            {"families", "vacation resort"},
+            {"families", "christmas celebration"},
+            {"laborers", "construction side"},
+            {"politicians", "debate"},
+        }
+
+        local i = math.random(1,5)
+        local ship = Ship:enrich(CpuShip():setTemplate("Personnel Freighter " .. i):setWarpDrive(true):setFaction("Human Navy"))
+        Util.spawnAtStation(fromStation, ship)
+        local passengers = Util.random(passengerList)
+        local number = math.random(1, 5) * (i-1) + math.random(1,5)
+
+        Ship:patrol(ship, {
+            {
+                target = toStation,
+                onHeading = function()
+                    ship:setDescription(
+                        "There are " ..
+                                number ..
+                                " " ..
+                                passengers[1] ..
+                                " on board going to a " ..
+                                passengers[2] ..
+                                " on " ..
+                                toStation:getCallSign() ..
+                                ".")
+                end,
+                onArrival = function()
+                    ship:destroy()
+                end
+            }
+        })
+    end,
+}
+
+function narrative(fromStation, toStation)
+    return Util.random(narratives)(fromStation, toStation)
 end
