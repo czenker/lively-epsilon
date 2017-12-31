9af603040050e03ba3a30640b8a4b86c1220a4c8
Christian Zenker <dev@xopn.de>
2018-01-01 00:54:46 +0100

    clean up code in player.lua

--- src/lively_epsilon/domain/player.lua
+++ src/lively_epsilon/domain/player.lua
@@ -16,28 +16,27 @@ local prototype = {
     end
 }
 
+Player = Player or {}
 
-Player = {
-    -- enrich a PlayerSpaceship with more story driven properties
-    enrich = function(self, player)
-        if not isEePlayer(player) then
-            error("player given to Player.enrich needs to be a PlayerSpaceship", 2)
-        end
-
-        if (player.isEnriched == true) then return player end
-
-        for key, value in pairs(prototype) do
-            player[key] = value
-        end
-
-        Cron.regular("startMission" .. player:getCallSign(), function()
-            if player.mission ~= nil and player.missionStarted == false then
-                print("starting mission '" .. player.mission.title .. "'")
-                player.mission:start(player)
-                player.missionStarted = true
-            end
-        end, 0.5)
-
-        return player
+-- enrich a PlayerSpaceship with more story driven properties
+Player.enrich = function(self, player)
+    if not isEePlayer(player) then
+        error("player given to Player.enrich needs to be a PlayerSpaceship", 2)
     end
-}
+
+    if (player.isEnriched == true) then return player end
+
+    for key, value in pairs(prototype) do
+        player[key] = value
+    end
+
+    Cron.regular("startMission" .. player:getCallSign(), function()
+        if player.mission ~= nil and player.missionStarted == false then
+            print("starting mission '" .. player.mission.title .. "'")
+            player.mission:start(player)
+            player.missionStarted = true
+        end
+    end, 0.5)
+
+    return player
+end
