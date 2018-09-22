d14e89e828f0a790ff473a6af125b585b0a08e5c
Christian Zenker <dev@xopn.de>
2018-09-22 20:33:10 +0200

    simplify ensureComms

--- src/lively_epsilon/tools/comms.lua
+++ src/lively_epsilon/tools/comms.lua
@@ -1,15 +1,15 @@
 Tools = Tools or {}
 
+-- makes sure the player is able to at least once accept a communication
+
 Tools.ensureComms = function(self, shipTemplateBased, player, description)
     if not isEeShipTemplateBased(shipTemplateBased) then error("Expected a station or ship, but got " .. type(shipTemplateBased), 2) end
     if not isEePlayer(player) then error("Expected a player, but got " .. type(player), 2) end
 
-    local cronId = Util.randomUuid()
-
-    Cron.regular(cronId, function()
+    Cron.regular(function(self)
         if player:isCommsInactive() then
             shipTemplateBased:sendCommsMessage(player, description)
-            Cron.abort(cronId)
+            Cron.abort(self)
         end
     end, 0.1, 0)
 end
