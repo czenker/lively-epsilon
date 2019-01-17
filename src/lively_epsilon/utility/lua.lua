2e410be914d5ad208d0abdb21010d2c6facd9b9f
Christian Zenker <dev@xopn.de>
2019-01-17 17:57:09 +0100

    wrapper to execute potentially unsafe user scripts

--- src/lively_epsilon/utility/lua.lua
+++ src/lively_epsilon/utility/lua.lua
@@ -4,3 +4,28 @@ function isFunction(thing) return type(thing) == "function" end
 function isBoolean(thing) return type(thing) == "boolean" end
 function isTable(thing) return type(thing) == "table" end
 function isNil(thing) return type(thing) == "nil" end
+
+-- try to call a user callback. If it causes an error, print it, but don't propagate it further
+function userCallback(func, ...)
+    if isFunction(func) then
+        local args = {...}
+        local result = table.pack(pcall(func, table.unpack(args)))
+        local success = table.remove(result, 1)
+        if not success then
+            local error = table.remove(result, 1)
+            local msg = "An error occured when calling a user function"
+            if type(error) == "string" then
+                msg = msg .. ": " .. error
+            end
+            logError(msg)
+            return nil
+        else
+            return table.unpack(result)
+        end
+    else
+        if not isNil(func) then
+            logError("Expected a function as callback, but got " .. type(func))
+        end
+        return nil
+    end
+end
---
+++ test/utility/lua_spec.lua
@@ -0,0 +1,41 @@
+insulate("Util", function()
+
+    require "lively_epsilon"
+    require "test.mocks"
+    require "test.asserts"
+    require "test.log_catcher"
+
+    describe("userCallback()", function()
+        it("calls the callback and returns the args if no error occurs", function()
+            local a,b,c = userCallback(function(arg1, arg2, arg3) return arg1 * 7, arg2 .. "bar", not arg3 end, 6, "foo", true)
+
+            assert.is_same(42, a)
+            assert.is_same("foobar", b)
+            assert.is_false(c)
+        end)
+
+        it("logs an error and returns nil if an error occurs", function()
+            withLogCatcher(function(logs)
+                local result = userCallback(function() error("Fail") end)
+                assert.is_nil(result)
+                assert.is_not_nil(logs:popLastError())
+            end)
+        end)
+
+        it("returns if no function is given", function()
+            withLogCatcher(function(logs)
+                local result = userCallback(nil)
+                assert.is_nil(result)
+                assert.is_nil(logs:popLastError())
+            end)
+        end)
+
+        it("logs an error if an invalid function is given", function()
+            withLogCatcher(function(logs)
+                local result = userCallback("foobar")
+                assert.is_nil(result)
+                assert.is_not_nil(logs:popLastError())
+            end)
+        end)
+    end)
+end)
