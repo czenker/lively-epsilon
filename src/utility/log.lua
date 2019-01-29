cdbda9aa6ef3a539974202c5fa1734a39e7734bf
Christian Zenker <dev@xopn.de>
2019-01-30 00:00:04 +0100

    add option to log game time in logs

--- src/utility/log.lua
+++ src/utility/log.lua
@@ -8,6 +8,14 @@ local function logLevel()
     return logLvl
 end
 
+local function timestamp()
+    if (LivelyEpsilonConfig or {}).logTime == true then
+        return string.format("[%7.1f]", Cron.now())
+    else
+        return ""
+    end
+end
+
 local red = "\u{001b}[41m\u{001b}[37m"
 local yellow = "\u{001b}[33m"
 local cyan = "\u{001b}[36m"
@@ -17,27 +25,27 @@ local reset = "\u{001b}[0m"
 logError = function (message)
     local logLvl = logLevel()
     if logLvl == nil or logLvl == "DEBUG" or logLvl == "INFO" or logLvl == "WARNING" or logLvl == "ERROR" then
-        print((useAnsi() and red or "") .. "[ERROR] " .. message .. (useAnsi() and reset or ""))
+        print(timestamp() .. (useAnsi() and red or "") .. "[ERROR] " .. message .. (useAnsi() and reset or ""))
     end
 end
 
 logWarning = function (message)
     local logLvl = logLevel()
     if logLvl == nil or logLvl == "DEBUG" or logLvl == "INFO" or logLvl == "WARNING" then
-        print((useAnsi() and yellow or "") .. "[WARN]  " .. message .. (useAnsi() and reset or ""))
+        print(timestamp() .. (useAnsi() and yellow or "") .. "[WARN]  " .. message .. (useAnsi() and reset or ""))
     end
 end
 
 logInfo = function (message)
     local logLvl = logLevel()
     if logLvl == nil or logLvl == "DEBUG" or logLvl == "INFO" then
-        print((useAnsi() and cyan or "") .. "[INFO]  " .. message .. (useAnsi() and reset or ""))
+        print(timestamp() .. (useAnsi() and cyan or "") .. "[INFO]  " .. message .. (useAnsi() and reset or ""))
     end
 end
 
 logDebug = function (message)
     local logLvl = logLevel()
     if logLvl == nil or logLvl == "DEBUG" then
-        print((useAnsi() and grey or "") .. "[DEBUG] " .. message .. (useAnsi() and reset or ""))
+        print(timestamp() .. (useAnsi() and grey or "") .. "[DEBUG] " .. message .. (useAnsi() and reset or ""))
     end
 end
