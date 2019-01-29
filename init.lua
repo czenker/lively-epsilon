98c0b8a8caea38069f2c8957d1aa9ae904eb7aa6
Christian Zenker <dev@xopn.de>
2019-01-29 23:25:32 +0100

    make the inclusion of the lib more portable outside EE

--- init.lua
+++ init.lua
@@ -100,6 +100,13 @@ local myPackages = {
 }
 
 if package ~= nil and package.path ~= nil then
+    local basePath = debug.getinfo(1).source
+    if basePath:sub(1,1) == "@" then basePath = basePath:sub(2) end
+    if basePath:sub(-8) == "init.lua" then basePath = basePath:sub(1, -9) end
+    basePath = "./" .. basePath
+
+    package.path = package.path .. ";" .. basePath .. "?.lua"
+
     -- when running tests
     local prefix = "lively_epsilon/"
 
