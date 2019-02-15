0ea62fac36e053fc3e01f8d68ccb0bfac0d6d8da
Christian Zenker <dev@xopn.de>
2019-02-15 21:41:17 +0100

    show "No Reply" screen when station or ship is not answering

--- src/scripts/comms.lua
+++ src/scripts/comms.lua
@@ -2,13 +2,16 @@ require "lively_epsilon/src/utility/lua.lua"
 require "lively_epsilon/src/domain/trait/shipTemplateBased/comms.lua"
 
 local function printScreen(screen)
+    local isCommsEmpty = true
     local whatNpcSays = screen:getWhatNpcSays(comms_target, player)
     if whatNpcSays ~= nil and whatNpcSays ~= "" then
+        isCommsEmpty = false
         setCommsMessage(whatNpcSays)
     end
     for _, reaction in pairs(screen:getHowPlayerCanReact()) do
         local visible = reaction:checkCondition(comms_target, player)
         if visible then
+            isCommsEmpty = false
             local playerSays = reaction:getWhatPlayerSays(comms_target, player)
             local goToNextScreen = function()
                 local nextScreen = reaction:getNextScreen(comms_target, player)
@@ -21,10 +24,12 @@ local function printScreen(screen)
             addCommsReply(playerSays, goToNextScreen)
         end
     end
+    if isCommsEmpty then
+        error("Intentional error to trigger the \"No Reply\" screen in Comms.")
+    end
 end
 
-
-function mainMenu()
+local function mainMenu()
     if (ShipTemplateBased:hasComms(comms_target)) then
         printScreen(comms_target:getComms(player))
     end
